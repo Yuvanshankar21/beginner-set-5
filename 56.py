4f7bec5a05b12aa69d94da636c925d276a252315
Yuvanshankar21 <52278651+Yuvanshankar21@users.noreply.github.com>
2019-07-21 03:16:49 -0700

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,13 @@
+a=(input())
+l=len(a)
+count=0
+sum1=0
+for x in range (l):
+  if (a[x].isdigit()):
+    count=count+1
+  elif(a[x].isalpha()):
+    sum1=sum1+1
+if(count!=0 and sum1!=0):    
+  print('yes')
+else:
+  print('no')    
